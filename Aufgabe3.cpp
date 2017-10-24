b371c841a61cbc99925d500f03806618d42b55b8
Alexander Kern <al-kern@web.de>
2017-10-24 15:51:55 +0200

    additional exercise 3

--- Aufgabe3.cpp
+++ Aufgabe3.cpp
@@ -25,7 +25,8 @@ namespace
                 add< double >("Threshold", "Grenzwert", 0.0008);
                 add< bool >("Center", "Zeichne am Ursprung", false); //TODO funktioniert noch nicht so ganz, vielleicht Loop-Reihenfolge? wahrsch. wegen Minimalpunktberechnung
                 //setDisabled("Center"); //bei Abgabe deaktiviert, falls es nicht klappt
-                add< double >("Radius", "Kugelradius", 1.0);
+                add< double >("Radius", "Kugelradius", 0.15);
+                add< bool >("Scale", "Größe der Punkte anhand ihres Wertes skalieren", false);
             }
         };
 
@@ -53,37 +54,33 @@ namespace
             std::unique_ptr< TensorFieldDiscrete< Scalar >::DiscreteEvaluator > evaluator = data->makeDiscreteEvaluator();
             auto numValues = evaluator->numValues(); //Anzahl der Punkte
             long dim = cbrt(numValues); //Dimension der Punktverteilung, im Beispiel 31
-            debugLog() << "NumValues: " << numValues << std::endl;
-            debugLog() << "Dimension: " << dim << std::endl;
             auto threshold = options.get< double >("Threshold");
 
-            //merke die initialen Koordinaten und ziehe sie beim zeichnen ab, um Punkte am Ursprung zu zeichnen
-            //irgendwie noch -0.5*dim um nicht nur im positiven zu zeichnen?
-            bool shouldCenter = options.get< bool >("Center");
-            unsigned int offsetX = dim;
-            unsigned int offsetY = dim;
-            unsigned int offsetZ = dim;
-
-            std::vector<Point3> points;
+            //Punkte in verschiedenen Listen speichern, abhängig von der Größe
+            std::unordered_map<double, std::vector<Point3>> points;
             for (unsigned int z = 0.0; z < dim; z++) {
                 for (unsigned int x = 0.0; x < dim; x++) {
                     for (unsigned int y = 0.0; y < dim; y++) {
                         auto index = z * dim * dim + x * dim + y;
                         auto value = evaluator->value(index);
                         if (value[0] >= threshold) {
-                            if (shouldCenter) {
-                                offsetX = std::min(offsetX, x);
-                                offsetY = std::min(offsetY, y);
-                                offsetZ = std::min(offsetZ, z);
-                                points.push_back(Point3(x - offsetX, y - offsetY, z - offsetZ));
-                                continue;
+                            double diameter = value[0] / threshold;
+                            if (points.find(diameter) == points.end()) {
+                                points.insert({diameter, std::vector<Point3>()});
                             }
-                            points.push_back(Point3(x, y, z));
+                            points[diameter].push_back(Point3(x, y, z));
                         }
                     }
                 }
             }
-            mGlyphs->add(Primitive::POINTS).setPointSize(options.get< double >("Radius")).setColor(options.get< Color >("Color")).setVertices(points);
+            Color color = options.get< Color >("Color");
+            double radius = options.get< double >("Radius");
+            double radToDraw;
+            for (auto& p : points) {
+                if (options.get< bool >("Scale")) radToDraw = p.first * radius;
+                else radToDraw = radius;
+                mGlyphs->add(Primitive::POINTS).setPointSize(radToDraw).setColor(color).setVertices(p.second);
+            }
         }
 
     };
