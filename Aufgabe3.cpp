c470fdf5728315f21347ced4c18fa195fd1282c9
Alexander Kern <al-kern@web.de>
2017-10-25 13:16:27 +0200

    new solution for exercise 3

--- Aufgabe3.cpp
+++ Aufgabe3.cpp
@@ -1,8 +1,6 @@
 #include <fantom/algorithm.hpp>
 #include <fantom/register.hpp>
 #include <fantom/fields.hpp>
-#include <fantom/dataset/TensorFieldBase.hpp>
-#include <fantom/datastructures/DataObjectBundle.hpp>
 #include <fantom/graphics.hpp>
 #include <math.h>
 
@@ -23,10 +21,9 @@ namespace
                 //add< DataObject >("Field", "Feld mit Input" );
                 add< Color >("Color", "Zeichenfarbe", Color(0.75, 0.75, 0.0));
                 add< double >("Threshold", "Grenzwert", 0.0008);
-                add< bool >("Center", "Zeichne am Ursprung", false); //TODO funktioniert noch nicht so ganz, vielleicht Loop-Reihenfolge? wahrsch. wegen Minimalpunktberechnung
-                //setDisabled("Center"); //bei Abgabe deaktiviert, falls es nicht klappt
                 add< double >("Radius", "Kugelradius", 0.15);
                 add< bool >("Scale", "Größe der Punkte anhand ihres Wertes skalieren", false);
+                add< bool >("new", "Zweite Implementierung", false);
             }
         };
 
@@ -45,38 +42,61 @@ namespace
         void execute( const Algorithm::Options& options, const volatile bool& /* abortFlag */ ) override
         {
             mGlyphs = getGraphics("Glyphs").makePrimitive();
-            std::shared_ptr< const TensorFieldDiscrete < Scalar > > data = options.get< TensorFieldDiscrete < Scalar > >("Field");
+            std::shared_ptr< const TensorFieldDiscrete < Scalar > > field = options.get< TensorFieldDiscrete < Scalar > >("Field");
 
-            if (!data) {
+            if (!field) {
                 return; //falls Field noch nicht vorhanden (zb beim Laden) abbrechen, sonst segfault
             }
 
-            std::unique_ptr< TensorFieldDiscrete< Scalar >::DiscreteEvaluator > evaluator = data->makeDiscreteEvaluator();
-            auto numValues = evaluator->numValues(); //Anzahl der Punkte
-            long dim = cbrt(numValues); //Dimension der Punktverteilung, im Beispiel 31
+            //Punkte in verschiedenen Listen speichern, abhängig von der Größe
+            std::unordered_map<double, std::vector<Point3>> valuesToDraw;
+            std::unique_ptr< TensorFieldDiscrete< Scalar >::DiscreteEvaluator > evaluator = field->makeDiscreteEvaluator();
             auto threshold = options.get< double >("Threshold");
 
-            //Punkte in verschiedenen Listen speichern, abhängig von der Größe
-            std::unordered_map<double, std::vector<Point3>> points;
-            for (unsigned int z = 0.0; z < dim; z++) {
-                for (unsigned int x = 0.0; x < dim; x++) {
-                    for (unsigned int y = 0.0; y < dim; y++) {
-                        auto index = z * dim * dim + x * dim + y;
-                        auto value = evaluator->value(index);
-                        if (value[0] >= threshold) {
-                            double diameter = value[0] / threshold;
-                            if (points.find(diameter) == points.end()) {
-                                points.insert({diameter, std::vector<Point3>()});
+            if (options.get< bool >("new")) { //zweite impl, zeichnet am Ursprung
+                std::shared_ptr< const Grid< 3 > > grid = std::dynamic_pointer_cast< const Grid< 3 > >( field->domain() );
+                const ValueArray< Point3 >& points = grid->points();
+
+                debugLog() << "Points: " << points.size() << std::endl;
+                debugLog() << "Values: " << evaluator->numValues() << std::endl;
+
+                auto size = points.size();
+                for (int i = 0; i < size; i++) {
+                    auto value = evaluator->value(i);
+                    if (value[0] >= threshold) {
+                        double diameter = value[0] / threshold;
+                        if (valuesToDraw.find(diameter) == valuesToDraw.end()) {
+                            valuesToDraw.insert({diameter, std::vector<Point3>()});
+                        }
+                        valuesToDraw[diameter].push_back(points[i]);
+                    }
+                }
+            }
+            else { //erste impl
+                auto numValues = evaluator->numValues(); //Anzahl der Punkte
+                long dim = cbrt(numValues); //Dimension der Punktverteilung, im Beispiel 31
+
+                for (unsigned int z = 0.0; z < dim; z++) {
+                    for (unsigned int x = 0.0; x < dim; x++) {
+                        for (unsigned int y = 0.0; y < dim; y++) {
+                            auto index = z * dim * dim + x * dim + y;
+                            auto value = evaluator->value(index);
+                            if (value[0] >= threshold) {
+                                double diameter = value[0] / threshold;
+                                if (valuesToDraw.find(diameter) == valuesToDraw.end()) {
+                                    valuesToDraw.insert({diameter, std::vector<Point3>()});
+                                }
+                                valuesToDraw[diameter].push_back(Point3(x, y, z));
                             }
-                            points[diameter].push_back(Point3(x, y, z));
                         }
                     }
                 }
             }
+
             Color color = options.get< Color >("Color");
             double radius = options.get< double >("Radius");
             double radToDraw;
-            for (auto& p : points) {
+            for (auto& p : valuesToDraw) {
                 if (options.get< bool >("Scale")) radToDraw = p.first * radius;
                 else radToDraw = radius;
                 mGlyphs->add(Primitive::POINTS).setPointSize(radToDraw).setColor(color).setVertices(p.second);
