0c1289e2ca1bafb25321ae13dab588f20c42280d
Alexander Kern <al-kern@web.de>
2017-11-01 15:00:54 +0100

    added selection options to ex4

--- Aufgabe4.cpp
+++ Aufgabe4.cpp
@@ -31,7 +31,7 @@ namespace
         ~Euler() {}
 
         Tensor<T, N> nextStep(Tensor<T, N> xn, TensorFieldContinuous<3, Tensor<double, 3>>::Evaluator& evaluator) override {
-            if (evaluator.reset(xn, 0)) {
+            if (evaluator.reset(xn)) {
                 auto vXn = evaluator.value(); //Wert an der Stelle xn
                 auto nextValue = xn + stepWidth * vXn; //Addition und Multiplikation auf Tensoren ist definiert
                 if (xn == nextValue) doesHaveNext = false;
@@ -41,16 +41,16 @@ namespace
             return xn;
         }
 
-        bool hasNext() {
+        bool hasNext() override {
             return doesHaveNext;
         }
 
-        void reset() {
+        void reset() override {
             doesHaveNext = true;
         }
     };
 
-    class VisThresholdAlgorithm : public VisAlgorithm
+    class VisStreamLinesAlgorithm : public VisAlgorithm
     {
         std::unique_ptr< Primitive > mGlyphs;
 
@@ -60,7 +60,25 @@ namespace
             Options( Control& control ) : VisAlgorithm::Options( control )
             {
                 add< TensorFieldContinuous< 3, Vector3 > >("Field", "Feld mit Input" ); ///home/visprak11/fantom/TestData/streamTest2.vtk
-                add< double >("Stepwidth", "Schrittweite für das Euler-Verfahren", 1.0);
+                add< double >("Stepwidth", "Schrittweite fuer das Euler-Verfahren", 1.0);
+
+                addSeparator();
+                add< double >("Startline_start_x", "2D-Linie Startpunkt x", -1);
+                add< double >("Startline_start_y", "2D-Linie Startpunkt y", 1);
+                add< double >("Startline_start_z", "2D-Linie Startpunkt z", 6);
+
+                addSeparator();
+                add< double >("Startline_end_x", "2D-Linie Endpunkt x", -1);
+                add< double >("Startline_end_y", "2D-Linie Endpunkt y", 1);
+                add< double >("Startline_end_z", "2D-Linie Endpunkt z", 0);
+
+                addSeparator();
+                add< double >("Startline_3D_x", "3D-Linie Startpunkt x", -5);
+                add< double >("Startline_3D_y", "3D-Linie Startpunkt y", 5);
+                add< double >("Startline_3D_z", "3D-Linie Startpunkt z", 6);
+
+                addSeparator();
+                add< int >("Number", "Anzahl an Punkten auf den Startlinien", 10);
             }
         };
 
@@ -72,7 +90,7 @@ namespace
             }
         };
 
-        VisThresholdAlgorithm( InitData & data) : VisAlgorithm( data )
+        VisStreamLinesAlgorithm( InitData & data) : VisAlgorithm( data )
         {
         }
 
@@ -82,26 +100,47 @@ namespace
             auto field = options.get< TensorFieldContinuous< 3, Vector3 > >("Field");
 
             if (!field) return;
-
-            auto evaluator = field->makeEvaluator();
-            //std::shared_ptr< const Grid< 3 > > grid = std::dynamic_pointer_cast< const Grid< 3 > >( field->domain() );
-            //const ValueArray< Point3 >& points = grid->points();
+//            std::shared_ptr< const Grid< 3 > > grid = std::dynamic_pointer_cast< const Grid< 3 > >( field->domain() );
+//            const ValueArray< Point3 >& points = grid->points();
 
             auto euler = Euler<double, 3>(options.get< double >("Stepwidth"));
-            Vector3 v = Point3(-4, 4, 4);
 
-            std::vector<Point3> vertices;
-            while (euler.hasNext()) {
-                Vector3 nv = euler.nextStep(v, *evaluator);
-                vertices.push_back(v);
-                vertices.push_back(nv);
-                v = nv;
+            std::vector<Point3> startPoints;
+
+            //Berechnung der Fläche für Startpunkte
+            Point3 startPoint(options.get<double>("Startline_start_x"), options.get<double>("Startline_start_y"), options.get<double>("Startline_start_z"));
+            Point3 endPoint(options.get<double>("Startline_end_x"), options.get<double>("Startline_end_y"), options.get<double>("Startline_end_z"));
+            Point3 startPoint3D(options.get<double>("Startline_3D_x"), options.get<double>("Startline_3D_y"), options.get<double>("Startline_3D_z"));
+            double numberPoints = options.get<int>("Number") - 1; //In Schleife wird einmal öfter iteriert, um Punkte auf den Grenzlinien alle dabei zu haben, deshalb -1
+
+            Point3 diff2D = endPoint - startPoint;
+            Point3 diff3D = startPoint3D - startPoint;
+            for (double i = 0; i <= numberPoints; i++) {
+                Point3 tempPoint2D = startPoint + (1/numberPoints) * i * diff2D;
+                for (double j = 0; j <= numberPoints; j++) {
+                    startPoints.push_back(tempPoint2D + (1/numberPoints) * j * diff3D);
+                }
             }
+            mGlyphs->add(Primitive::POINTS).setColor(Color(1, 0, 0)).setPointSize(4).setVertices(startPoints);
 
-            mGlyphs->add(Primitive::LINES).setVertices(vertices);
+            for (size_t i = 0; i < startPoints.size(); i++) {
+                auto evaluator = field->makeEvaluator();
+                auto v = startPoints[i];
+                std::vector<Point3> vertices;
+
+                while (euler.hasNext()) {
+                    Vector3 nv = euler.nextStep(v, *evaluator);
+                    vertices.push_back(v);
+                    vertices.push_back(nv);
+                    v = nv;
+                }
+                euler.reset();
+
+                mGlyphs->add(Primitive::LINES).setVertices(vertices);
+            }
         }
 
     };
 
-    AlgorithmRegister< VisThresholdAlgorithm > reg("VisPraktikum/Aufgabe 4", "Zeichnet Stromlinien");
+    AlgorithmRegister< VisStreamLinesAlgorithm > reg("VisPraktikum/Aufgabe 4", "Zeichnet Stromlinien");
 }
