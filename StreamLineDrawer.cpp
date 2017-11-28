c2a6b2c3ae9b967999f3e883573ec849a46287d3
Alexander Kern <al-kern@web.de>
2017-11-28 14:19:50 +0100

    fixed streamlinedrawer for 2d field

--- StreamLineDrawer.cpp
+++ StreamLineDrawer.cpp
@@ -7,12 +7,11 @@ using namespace fantom;
 
 namespace
 {
-    template<class T, size_t N>
     class Integrator
     {
     public:
         virtual ~Integrator() {}
-        virtual Tensor<T, N> nextStep(Tensor<T, N> xn, TensorFieldContinuous<3, Tensor<double, 3>>::Evaluator& evaluator) = 0;
+        virtual Point2 nextStep(Point2 xn, TensorFieldContinuous<2, Point2>::Evaluator& evaluator) = 0;
         void reset() {setHasNext(true);}
         bool hasNext() {return dhn;}
     protected:
@@ -26,14 +25,13 @@ namespace
         bool dhn;
     };
 
-    template<class T, size_t N>
-    class Euler : public Integrator<T, N>
+    class Euler : public Integrator
     {
     public:
-        Euler(double initialStepWidth) : Integrator<T, N>(initialStepWidth) {}
+        Euler(double initialStepWidth) : Integrator(initialStepWidth) {}
         ~Euler() {}
 
-        Tensor<T, N> nextStep(Tensor<T, N> startPoint, TensorFieldContinuous<3, Tensor<double, 3>>::Evaluator& evaluator) override {
+        Point2 nextStep(Point2 startPoint, TensorFieldContinuous<2, Point2>::Evaluator& evaluator) override {
             if (evaluator.reset(startPoint)) {
                 auto changeRate = evaluator.value(); //Wert an der Stelle xn
                 auto nextPoint = startPoint + this->stepWidth * changeRate; //Addition und Multiplikation auf Tensoren ist definiert
@@ -45,25 +43,24 @@ namespace
         }
     };
 
-    template<class T, size_t N>
-    class RungeKutta : public Integrator<T, N> {
+    class RungeKutta : public Integrator {
     public:
-        RungeKutta(double initialStepWidth): Integrator<T, N>(initialStepWidth) {}
+        RungeKutta(double initialStepWidth): Integrator(initialStepWidth) {}
         ~RungeKutta() {}
 
-        Tensor<T, N> nextStep(Tensor<T, N> startPoint, TensorFieldContinuous<3, Tensor<double, 3>>::Evaluator& evaluator) override {
-            Tensor<T, N> q1 = getTensor(startPoint, evaluator);
-            Tensor<T, N> q2 = getTensor(startPoint + this->stepWidth * 0.5 * q1, evaluator);
-            Tensor<T, N> q3 = getTensor(startPoint + this->stepWidth * 0.5 * q2, evaluator);
-            Tensor<T, N> q4 = getTensor(startPoint + q3, evaluator);
-            Tensor<T, N> nextPoint = startPoint + this->stepWidth / 6 * (q1 + q2 + q3 + q4);
+        Point2 nextStep(Point2 startPoint, TensorFieldContinuous<2, Point2>::Evaluator& evaluator) override {
+            Point2 q1 = getTensor(startPoint, evaluator);
+            Point2 q2 = getTensor(startPoint + this->stepWidth * 0.5 * q1, evaluator);
+            Point2 q3 = getTensor(startPoint + this->stepWidth * 0.5 * q2, evaluator);
+            Point2 q4 = getTensor(startPoint + q3, evaluator);
+            Point2 nextPoint = startPoint + this->stepWidth / 6 * (q1 + q2 + q3 + q4);
             if (startPoint == nextPoint) this->setHasNext(false);
             if (!this->hasNext()) return startPoint;
             return nextPoint;
         }
 
     private:
-        Tensor<T, N> getTensor(Tensor<T, N> point, TensorFieldContinuous<3, Tensor<double, 3>>::Evaluator& evaluator) {
+        Point2 getTensor(Point2 point, TensorFieldContinuous<2, Point2>::Evaluator& evaluator) {
             if (evaluator.reset(point)) {
                 return evaluator.value();
             } else {
@@ -88,7 +85,7 @@ namespace
         {
             Options( Control& control ) : VisAlgorithm::Options( control )
             {
-                add< TensorFieldContinuous< 3, Vector3 > >("Field", "Feld mit Input" ); ///home/visprak11/fantom/TestData/streamTest2.vtk
+                add< TensorFieldContinuous<2, Vector2>>("Field", "Feld mit Input" ); ///home/visprak11/fantom/TestData/streamTest2.vtk
                 add< InputChoices >("Method", "Integrationsverfahren", std::vector<std::string>({"Euler", "Runge-Kutta"}), "Euler");
                 add< double >("Stepwidth", "Schrittweite fuer das Euler-Verfahren", 1.0); //TODO disable for Runge-Kutta
                 add< Color >("Color", "Farbe der Stromlinien", Color(0.75, 0.75, 0.0));
@@ -111,23 +108,24 @@ namespace
         void execute( const Algorithm::Options& options, const volatile bool& /* abortFlag */ ) override
         {
             mGlyphs = getGraphics("Streamlines").makePrimitive();
-            auto field = options.get< TensorFieldContinuous< 3, Vector3 > >("Field");
 
+            auto field = options.get<TensorFieldContinuous<2, Vector2>>("Field");
             if (!field) return;
 
-            Integrator<double, 3>* integrator;
-
-            std::string method = options.get< std::string >("Method");
-            if (method.compare("Euler") == 0) integrator = new Euler<double, 3>(options.get< double >("Stepwidth"));
-            if (method.compare("Runge-Kutta") == 0) integrator = new RungeKutta<double, 3>(options.get< double >("Stepwidth"));
-
-            //save Seedpoints in std::vector
             auto startPointsValueArray = options.get< DefaultValueArray<Point3> >("Seedpoints");
-            if (!startPointsValueArray) return;
             std::vector<Point3> startPoints;
-            for (size_t i = 0; i < startPointsValueArray->size(); i++) {
-                startPoints.push_back(startPointsValueArray->operator[](i));
+            if (startPointsValueArray) {
+                for (size_t i = 0; i < startPointsValueArray->size(); i++) {
+                    startPoints.push_back(startPointsValueArray->operator[](i));
+                }
+            } else {
+                startPoints.push_back(Point3(1.983000, 0, 0));
             }
+
+            Integrator* integrator;
+            std::string method = options.get< std::string >("Method");
+            if (method.compare("Euler") == 0) integrator = new Euler(options.get< double >("Stepwidth"));
+            if (method.compare("Runge-Kutta") == 0) integrator = new RungeKutta(options.get< double >("Stepwidth"));
 
             mGlyphs->add(Primitive::POINTS).setColor(Color(1, 0, 0)).setPointSize(4).setVertices(startPoints);
 
@@ -137,7 +135,7 @@ namespace
                 std::vector<Point3> vertices;
 
                 while (integrator->hasNext()) {
-                    Vector3 nextPoint = integrator->nextStep(startPoint, *evaluator);
+                    Vector3 nextPoint = to3D(integrator->nextStep(to2D(startPoint), *evaluator));
                     vertices.push_back(startPoint);
                     vertices.push_back(nextPoint);
                     startPoint = nextPoint;
@@ -146,6 +144,14 @@ namespace
 
                 mGlyphs->add(Primitive::LINES).setColor(options.get< Color >("Color")).setVertices(vertices);
             }
+        }
+
+        Point2 to2D(Point3 p) {
+            return Point2(p[0], p[1]);
+        }
+
+        Point3 to3D(Point2 p) {
+            return Point3(p[0], p[1], 0);
         }
 
     };
--- test.cpp
+++ test.cpp
@@ -10,6 +10,36 @@ using namespace fantom;
 
 namespace
 {
+
+class Euler
+{
+    public:
+        Euler(double initialStepWidth) {
+            this->stepWidth = initialStepWidth;
+            this->reset();
+        }
+        ~Euler() {}
+
+        Point2 nextStep(Point2 startPoint, TensorFieldContinuous<2, Point2>::Evaluator& evaluator) {
+            if (evaluator.reset(startPoint)) {
+                auto changeRate = evaluator.value(); //Wert an der Stelle xn
+                auto nextPoint = startPoint + this->stepWidth * changeRate; //Addition und Multiplikation auf Tensoren ist definiert
+                if (startPoint == nextPoint) this->setHasNext(false);
+                return nextPoint;
+            }
+            this->setHasNext(false);
+            return startPoint;
+        }
+
+        void reset() {setHasNext(true);}
+        bool hasNext() {return dhn;}
+    protected:
+        void setHasNext(bool b) {dhn = b;}
+        double stepWidth;
+    private:
+        bool dhn;
+    };
+
     class VisThresholdAlgorithm : public VisAlgorithm
     {
         std::unique_ptr< Primitive > mGlyphs;
@@ -19,7 +49,7 @@ namespace
         {
             Options( Control& control ) : VisAlgorithm::Options( control )
             {
-
+                add< TensorFieldContinuous< 2, Vector2 > >("Field", "Feld mit Input" );
             }
         };
 
@@ -27,7 +57,7 @@ namespace
         {
             VisOutputs( fantom::VisOutputs::Control& control ) : VisAlgorithm::VisOutputs( control )
             {
-                addGraphics( "Glyphs" );
+                addGraphics( "Streamlines" );
             }
         };
 
@@ -37,17 +67,41 @@ namespace
 
         void execute( const Algorithm::Options& options, const volatile bool& /* abortFlag */ ) override
         {
-            mGlyphs = getGraphics("Glyphs").makePrimitive();
-            std::vector<Point3> points;
-            points.push_back(Point3(0.1, 0.1, 0.1));
+            mGlyphs = getGraphics("Streamlines").makePrimitive();
+            auto field = options.get< TensorFieldContinuous< 2, Vector2 > >("Field");
 
-            std::vector<Point3> lines;
-            points.push_back(Point3(0.0, 0.0, 0.0));
-            points.push_back(Point3(0.05, 0.05, 0.05));
-            //points.push_back(Point3(0.0, 1.0, 0.0));
+            if (!field) return;
+            auto evaluator = field->makeEvaluator();
 
-            mGlyphs->add(Primitive::LINES).setColor(Color(0.5,0.5,0.5)).setVertices(lines);
-            mGlyphs->add(Primitive::POINTS).setColor(Color(0.5,0.5,0.5)).setVertices(points);
+            Euler integrator(0.01);
+            std::vector<Point3> vertices;
+
+            Point3 startPoint(1.983000, 0, 0);
+            //Point3 startPoint(-3, 3, 3);
+            Point3 p(0,0,0);
+            std::vector<Point3> startPoints;
+            startPoints.push_back(startPoint);
+            startPoints.push_back(p);
+            mGlyphs->add(Primitive::POINTS).setPointSize(4).setColor(Color(0,1,0)).setVertices(startPoints);
+
+            Point2 sp = to2D(startPoint);
+            while (integrator.hasNext()) {
+                Vector2 nextPoint = integrator.nextStep(sp, *evaluator);
+                vertices.push_back(to3D(sp));
+                vertices.push_back(to3D(nextPoint));
+                sp = nextPoint;
+            }
+            integrator.reset();
+
+            mGlyphs->add(Primitive::LINES).setColor(Color(1,0,0)).setVertices(vertices);
+        }
+
+        Point2 to2D(Point3 p) {
+            return Point2(p[0], p[1]);
+        }
+
+        Point3 to3D(Point2 p) {
+            return Point3(p[0], p[1], 0);
         }
 
     };
